53402a0e422a6ad8dbd0cb957f1b85e13a136394
Its-Maeae <mariusschmi1711@gmail.com>
2026-03-14 20:13:33 +0100

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ import '../styles.css/qr_code.css';
 import '../styles.css/statistics.css';
 import '../styles.css/SecurityDashboard.css';
 import '../styles.css/undetermined.css';
+import '../styles.css/add_minerals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
